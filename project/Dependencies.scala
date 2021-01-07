bdfe95089c913f46aef75bdcf3a9e6e1e31c38e3
Scala Steward <me@scala-steward.org>
2021-01-07 14:55:08 +0100

    Update google-api-services-youtube to v3-rev20201214-1.31.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
         ExclusionRule("org.mortbay.jetty", "servlet-api")
       ),
     "com.softwaremill.retry" %% "retry" % "0.3.3",
-    "com.google.apis" % "google-api-services-youtube" % "v3-rev212-1.25.0"
+    "com.google.apis" % "google-api-services-youtube" % "v3-rev20201214-1.31.0"
   )
 
 }

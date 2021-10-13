62b69913f33422012aeb2381167933bfb5b96694
Scala Steward <me@scala-steward.org>
2021-10-13 20:20:14 +0200

    Update spark-streaming to 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
       .excludeAll(
         ExclusionRule("io.netty")
       ),
-    ("org.apache.spark" %% "spark-streaming" % "3.1.2")
+    ("org.apache.spark" %% "spark-streaming" % "3.2.0")
       .excludeAll(
         ExclusionRule("org.apache.hadoop"),
         ExclusionRule("jakarta.xml.bind"),

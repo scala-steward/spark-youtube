f423bed96ab8b25af95ae2a646071e591e7b1191
Scala Steward <scala_steward@virtuslab.com>
2022-08-11 05:06:58 +0000

    Update google-api-services-youtube to v3-rev20220719-2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -51,7 +51,7 @@ object Dependencies {
         ExclusionRule("org.slf4j", "slf4j-log4j12")
       ),
     "com.softwaremill.retry" %% "retry" % "0.3.5",
-    ("com.google.apis" % "google-api-services-youtube" % "v3-rev20220719-1.32.1")
+    ("com.google.apis" % "google-api-services-youtube" % "v3-rev20220719-2.0.0")
       .excludeAll(
         ExclusionRule("commons-logging")
       ),
